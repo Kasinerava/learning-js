ab76bd7c0703f8731934db09c1054fa2d5b23dfa
Kasinerava <65457233+Kasinerava@users.noreply.github.com>
2021-04-24 03:54:27 +0300

    Task 1

---
+++ alert.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const arrProfiles = [];
+
+arrProfiles[0] = {
+    name: 'Darth Vader',
+    age: 41,
+    isEmpire: true,
+    interests: ['fight', 'power']
+}
+
+arrProfiles[1] = {
+    name: 'Princess Leia',
+    age: 60,
+    isEmpire: false,
+    interests: ['politics', 'peace']
+}
+
+arrProfiles[2] = {
+    name: 'R2-D2',
+    age: 10,
+    isEmpire: false,
+    interests: ['electronics', 'friends']
+};
+
+console.log(arrProfiles);
